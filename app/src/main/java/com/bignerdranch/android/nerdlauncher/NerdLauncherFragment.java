39db2564baefa05de373b4ca85e79c98dd85acbd
mehagar <michaelehagar@gmail.com>
2015-04-01 10:59:51 -0500

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.nerdlauncher;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by mehagar on 4/1/2015.
+ */
+public class NerdLauncherFragment extends ListFragment {
+}
